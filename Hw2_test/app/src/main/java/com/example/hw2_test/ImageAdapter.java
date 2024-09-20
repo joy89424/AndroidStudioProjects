593cf8f1b214f5f416fd462586f585acd01b81f4
Liao.Joy <liao.joy@tolka.tv>
2024-09-20 10:30:12 +0800

    20240920_1

--- Hw2_test/app/src/main/java/com/example/hw2_test/ImageAdapter.java
+++ Hw2_test/app/src/main/java/com/example/hw2_test/ImageAdapter.java
@@ -1,4 +1,4 @@
-package com.example.hw2;
+package com.example.hw2_test;
 
 import android.content.Context;
 import android.net.Uri;
--- Hw2_test/app/src/main/java/com/example/hw2_test/MainActivity.java
+++ Hw2_test/app/src/main/java/com/example/hw2_test/MainActivity.java
@@ -1,10 +1,9 @@
-package com.example.hw2;
+package com.example.hw2_test;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
