549a1193b8637f09134e6ad4851b3ce80bf09dc9
Liao.Joy <liao.joy@tolka.tv>
2024-10-15 18:32:59 +0800

    20241015_3

--- TestA_new/app/src/main/java/com/example/testa_new/MainActivity.java
+++ TestA_new/app/src/main/java/com/example/testa_new/MainActivity.java
@@ -9,7 +9,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowInsetsController;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -93,6 +95,21 @@ public class MainActivity extends AppCompatActivity {
         // 指定自定義的下拉菜單佈局樣式
         adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        /*
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, selectedItem + " selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+        */
 
         imagePickerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getData() != null) {
